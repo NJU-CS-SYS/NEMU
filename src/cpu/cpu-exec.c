dbdc0d9b3f99dd2b379cbf9067a4d0eb01269cfa
jyy <njujiangyy@gmail.com>
2014-09-26 18:49:17 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  18:49:17 up  4:28,  1 user,  load average: 0.08, 0.05, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/cpu/cpu-exec.c
+++ src/cpu/cpu-exec.c
@@ -51,7 +51,7 @@ void cpu_exec(volatile uint32_t n) {
 		int instr_len = exec(cpu.eip);
 
 		/* restore the breakpoint on the byte */
-		if (bp_state == RECOVER) reset(eip);
+		if (bp_state == RECOVER) reset_bp(cpu.eip);
 		
 		cpu.eip += instr_len;
 
