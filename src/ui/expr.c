0fce74481e5b4cbfbd344e9f1c68f0dca48e4e05
131220159-Wang Huizhe <huizhewang@outlook.com>
2014-10-04 16:34:48 +0800

    add token rules and complement make_token() note: without number token!

--- src/ui/expr.c
+++ src/ui/expr.c
@@ -3,6 +3,7 @@
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
  */
+
 #include <sys/types.h>
 #include <regex.h>
 
@@ -24,11 +25,18 @@ static struct rule {
 
 	{" +",	NOTYPE},				// white space
 	{"\\+", '+'},					// plus
+	{"\\-", '-'},                   // minus
+	{"\\*", '*'},                   // multiple
+	{"\\/", '/'},                   // divide
+	{"\\(", '('},                   // left bracket
+	{"\\)", ')'},                   // right bracket
 	{"==", EQ}						// equal
 };
 
+/* the size of regex(s) */
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
 
+/* regex buffer */
 static regex_t re[NR_REGEX];
 
 /* Rules are used for more times.
@@ -40,8 +48,10 @@ void init_regex() {
 	int ret;
 
 	for(i = 0; i < NR_REGEX; i ++) {
-		ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
+		/* compiling */
+		ret = regcomp(re+i, rules[i].regex, REG_EXTENDED);
 		if(ret != 0) {
+			/* if not success */
 			regerror(ret, &re[i], error_msg, 128);
 			test(0, "regex compilation failed: %s\n%s\n", error_msg, rules[i].regex);
 		}
@@ -50,11 +60,11 @@ void init_regex() {
 
 typedef struct token {
 	int type;
-	char str[32];
+	char str[32]; /* buffer */
 } Token;
 
 Token tokens[32];
-int nr_token;
+int nr_token; /* the number of token that have been recognized */
 
 static bool make_token(char *e) {
 	int position = 0;
@@ -66,7 +76,7 @@ static bool make_token(char *e) {
 	while(e[position] != '\0') {
 		/* Try all rules one by one. */
 		for(i = 0; i < NR_REGEX; i ++) {
-			if(regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+			if(regexec(re + i, e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
 				char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
 
@@ -78,10 +88,20 @@ static bool make_token(char *e) {
 				/* TODO: Now a new token is recognized with rules[i]. 
 				 * Add codes to perform some actions with this token.
 				 */
-
+				tokens[nr_token].type = rules[i].type;
+				if (substr_len < 32) {
+					strcpy(tokens[nr_token].str, substr_start);
+				} else {
+					printf("the substring of the tokens is too long!\n");
+					printf("position %d\n%s %s\n", position, rule[i].regex, substr_start);
+					assert(0);
+				}
+				
+				/* why ?
 				switch(rules[i].token_type) {
 					default: assert(0);
 				}
+				*/
 
 				break;
 			}
@@ -94,6 +114,14 @@ static bool make_token(char *e) {
 	}
 
 	return true; 
+}
+
+void test_tokens(char *e) {
+	make_token(e);
+	int i;
+	for (i = 0; i < nr_tokens; i++) {
+		printf("%s ", tokens[i].str);
+	}
 }
 
 uint32_t expr(char *e, bool *success) {
