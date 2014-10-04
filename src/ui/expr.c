6b3ab679d04c341ecd185f82b7d1771f44022d2d
jyy <njujiangyy@gmail.com>
2014-10-04 17:20:02 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  17:20:02 up  6:34,  2 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---vim      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/ui/expr.c
+++ src/ui/expr.c
@@ -118,9 +118,9 @@ static bool make_token(char *e) {
 
 void test_tokens(char *e) {
 	make_token(e);
-	assert(0);
 	int i;
 	for (i = 0; i < nr_token; i++) {
+		printf("test\n");
 		printf("%s ", tokens[i].str);
 	}
 }
