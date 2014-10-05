8851b076d51fa22c6adaf670600e0012c0c93153
jyy <njujiangyy@gmail.com>
2014-10-05 11:09:39 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  11:09:39 up 13:18,  2 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---vim      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/ui/expr.c
+++ src/ui/expr.c
@@ -29,7 +29,7 @@ static struct rule {
 	{"-", '-'},                     // minus
 	{"\\*", '*'},                   // multiple
 	{"/", '/'},                     // divide
-	{"_", '_'}                      // NEG
+	{"_", '_'},                     // NEG
 	{"\\(", '('},                   // left bracket
 	{"\\)", ')'},                   // right bracket
 	{"[0-9]+", NUM},                // numbers
@@ -108,7 +108,7 @@ static bool make_token(char *e) {
 							int temp_type = tokens[nr_token-1].type;
 							if (('0' <= temp_type && temp_type <= '9') || temp_type == ')') {
 								temp_token->type = '_';
-								srtcpy(temp_token->str, "_");
+								strcpy(temp_token->str, "_");
 							}
 						}
 					}
