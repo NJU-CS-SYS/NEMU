859bb433765e7db69bd2d16c55ba8a5c5d4686a6
jyy <njujiangyy@gmail.com>
2014-10-05 11:19:23 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  11:19:23 up 13:28,  2 users,  load average: 0.01, 0.02, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---vim      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/ui/expr.c
+++ src/ui/expr.c
@@ -106,7 +106,7 @@ static bool make_token(char *e) {
 						}
 						else { // focus on pre character
 							int temp_type = tokens[nr_token-1].type;
-							if (('0' <= temp_type && temp_type <= '9') || temp_type == ')') {
+							if (!(('0' <= temp_type && temp_type <= '9') || temp_type == ')')) {
 								temp_token->type = '_';
 								strcpy(temp_token->str, "_");
 							}
