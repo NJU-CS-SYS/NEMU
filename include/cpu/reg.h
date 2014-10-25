8e28e86572b827bb9d98f253e288a61d14ac4d79
131220159-Wang Huizhe <huizhewang@outlook.com>
2014-10-25 17:37:33 +0800

    Add parity() to check PF

--- include/cpu/reg.h
+++ include/cpu/reg.h
@@ -67,6 +67,14 @@ enum { CF, CONSERVE_1_1, PF, CONSERVE_0_2, AF, CONSERV_0_3, ZF, SF, TF, IF, DF, 
 #define FLAG_VAL(index) ((cpu.eflags & (0x1 << index)) == (0x1 << index))
 #define FLAG_CHG(index, val) (cpu.eflags = (val) ? (cpu.eflags | (0x1 << index)) : (cpu.eflags & (~(0x1 << index))))
 
+bool parity(uint32_t val) {
+	val = 0xff & val;
+	int c = 0;
+	for (; val != 0; val = val >> 1)
+		if (val & 0x1) c++;
+	return !(c & 0x1);
+}
+
 extern const char* regsl[];
 extern const char* regsw[];
 extern const char* regsb[];
