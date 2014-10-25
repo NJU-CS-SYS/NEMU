536610121e092d8783aacd96855216d4e221b442
131220159-Wang Huizhe <huizhewang@outlook.com>
2014-10-25 19:07:01 +0800

    two bytes opcode! how to do?

--- src/exec/control-transfer/jcc-template.h
+++ src/exec/control-transfer/jcc-template.h
@@ -4,7 +4,7 @@
 
 make_helper(concat(je_, SUFFIX)) {
 	DATA_TYPE disp = instr_fetch(eip + 1, DATA_BYTE);
-	print_asm("je $0x%x,", imm);
+	print_asm("je $0x%x,", disp);
 	if (FLAG_VAL(ZF)) cpu.eip += (int)disp; /* sign extended */
 	if (suffix == 'w') cpu.eip &= 0x0000ffff;
 	return DATA_BYTE + 1;
