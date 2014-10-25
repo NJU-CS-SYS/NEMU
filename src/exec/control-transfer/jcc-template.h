2037f74ecc1b62652a612a3ba9dc21c01f7e7b2d
131220159-Wang Huizhe <huizhewang@outlook.com>
2014-10-26 01:46:08 +0800

    Modify the print format of je
    withou $ and 0x, also 16 based

--- src/exec/control-transfer/jcc-template.h
+++ src/exec/control-transfer/jcc-template.h
@@ -4,9 +4,9 @@
 
 make_helper(concat(je_, SUFFIX)) {
 	DATA_TYPE disp = instr_fetch(eip + 1, DATA_BYTE);
-	print_asm("je $0x%x,", disp);
 	if (FLAG_VAL(ZF)) cpu.eip += (int)disp; /* sign extended */
 	if (suffix == 'w') cpu.eip &= 0x0000ffff;
+	print_asm("je %x,", disp + cpu.eip);
 	return DATA_BYTE + 1;
 }
 #include "exec/template-end.h"
