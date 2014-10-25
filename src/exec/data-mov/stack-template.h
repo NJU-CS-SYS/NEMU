c3743c9ef578447b1c0a3474b2f82a97ce3036ce
jyy <njujiangyy@gmail.com>
2014-10-25 13:47:25 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  13:47:25 up  1:33,  4 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-2*[sshd---sshd---zsh---vim]      `-udevd---2*[udevd]

--- src/exec/data-mov/stack-template.h
+++ src/exec/data-mov/stack-template.h
@@ -1,7 +1,6 @@
 #include "exec/helper.h"
 #include "exec/template-start.h"
 #include "cpu/modrm.h"
-#include "nemu.h"
 
 /* this head file deals with instructions
  * such as push and pop which operate the stack,
@@ -22,7 +21,7 @@ make_helper(concat(push_, SUFFIX)) {
 	 * Due to little endian, esp should get smaller.
 	 */
 	Log("R_ESP is %x and DATA_BYTE is %d at EIP %x instr %x", REG(R_ESP), DATA_BYTE, eip, MEM_R(eip));
-	MEM_W( REG(R_ESP) - DATA_BYTE, REG(reg_code) );
+	MEM_W( REG(4) - DATA_BYTE, REG(reg_code) );
 	REG(R_ESP) -= DATA_BYTE;
 
 	print_asm("push" str(SUFFIX) " %%%s", REG_NAME(reg_code));
