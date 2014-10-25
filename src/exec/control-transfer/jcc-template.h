e60f727c07f18c6aada42504d20097b0c68cbafe
jyy <njujiangyy@gmail.com>
2014-10-26 03:27:41 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  03:27:41 up  1:49,  1 user,  load average: 0.06, 0.04, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---sshd---zsh---make---sh-+-git      |                                      `-pstree      `-udevd---2*[udevd]

--- src/exec/control-transfer/jcc-template.h
+++ src/exec/control-transfer/jcc-template.h
@@ -6,7 +6,7 @@ make_helper(concat(je_, SUFFIX)) {
 	DATA_TYPE disp = instr_fetch(eip + 1, DATA_BYTE);
 	if (FLAG_VAL(ZF)) cpu.eip += (int)disp; /* sign extended */
 	if (DATA_BYTE == 2) cpu.eip &= 0x0000ffff;
-	print_asm("je %x,", cpu.eip + DATA_BYTE + 1);
+	print_asm("je %x", cpu.eip + DATA_BYTE + 1);
 	return DATA_BYTE + 1;
 }
 #include "exec/template-end.h"
