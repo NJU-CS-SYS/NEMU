8c1c7b594fa68982dabc89d2fd8aa713891767e9
jyy <njujiangyy@gmail.com>
2014-10-29 17:58:45 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  17:58:45 up 58 min,  5 users,  load average: 0.08, 0.03, 0.02 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---zsh---vim]      |      |-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-sshd---sshd---zsh      `-udevd---2*[udevd]

--- src/exec/binary-arithmetic/cmp-template.h
+++ src/exec/binary-arithmetic/cmp-template.h
@@ -10,31 +10,27 @@ make_helper(concat(cmp_i8_rm_, SUFFIX)) {
 	DATA_TYPE sub;
 	DATA_TYPE result;
 	m.val = instr_fetch(eip + 1, 1);
-	if (m.reg == 7) {
-		if(m.mod == 3) {
-			imm = instr_fetch(eip + 1 + 1, 1);
-			len += 1;
-			sub = REG(m.R_M);
-			print_asm("cmp" str(SUFFIX) " $0x%x,%%%s", imm, REG_NAME(m.R_M));
-		}
-		else {
-			swaddr_t addr;
-			len += read_ModR_M(eip + 1, &addr);
-			imm = instr_fetch(eip + 1 + len, 1);
-			sub = MEM_R(addr);
-			print_asm("cmp" str(SUFFIX) " $0x%x,%s", imm, ModR_M_asm);
-		}
-		result = sub - imm;
-		FLAG_CHG(OF, OVERFLOW(imm, sub, result));
-		FLAG_CHG(SF, MSB(result));
-		FLAG_CHG(ZF, result == 0);
-		FLAG_CHG(AF, ADJUST(imm, sub));
-		FLAG_CHG(PF, PARITY(result));
-		FLAG_CHG(CF, (sub - imm) < 0);
-		return len + 1; // 1 for opcode
+	if(m.mod == 3) {
+		imm = instr_fetch(eip + 1 + 1, 1);
+		len += 1;
+		sub = REG(m.R_M);
+		print_asm("cmp" str(SUFFIX) " $0x%x,%%%s", imm, REG_NAME(m.R_M));
 	}
-	assert(0);
-	return 1;
+	else {
+		swaddr_t addr;
+		len += read_ModR_M(eip + 1, &addr);
+		imm = instr_fetch(eip + 1 + len, 1);
+		sub = MEM_R(addr);
+		print_asm("cmp" str(SUFFIX) " $0x%x,%s", imm, ModR_M_asm);
+	}
+	result = sub - imm;
+	FLAG_CHG(OF, OVERFLOW(imm, sub, result));
+	FLAG_CHG(SF, MSB(result));
+	FLAG_CHG(ZF, result == 0);
+	FLAG_CHG(AF, ADJUST(imm, sub));
+	FLAG_CHG(PF, PARITY(result));
+	FLAG_CHG(CF, (sub - imm) < 0);
+	return len + 1; // 1 for opcode
 }
 
 #include "exec/template-end.h"
