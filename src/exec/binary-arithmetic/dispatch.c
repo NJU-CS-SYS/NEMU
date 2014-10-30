af08ee450331374607e8d15be020f1e0eee43e5b
jyy <njujiangyy@gmail.com>
2014-10-30 23:24:46 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  23:24:46 up  2:12,  3 users,  load average: 0.01, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-2*[sshd---sshd---zsh---vim]      `-udevd---2*[udevd]

--- src/exec/binary-arithmetic/dispatch.c
+++ src/exec/binary-arithmetic/dispatch.c
@@ -3,15 +3,9 @@
 #include "cpu/modrm.h"
 
 #define GET_REG ModR_M m; m.val = instr_fetch(eip + 1, 1)
-make_helper(cmp_i8_rm_v);
-make_helper(reg_83_v) {
-	GET_REG;
-	switch (m.reg) {
-		case 7: return cmp_i8_rm_v(eip);
-	}
-	test(0, "Invalid modrm reg domain");
-	return 0;
-}
+#define FAIL_DISPATCH \
+test(0, "Invalid modrm reg domain");\
+return 0
 
 make_helper(sub_i2rm_b);
 make_helper(cmp_i8_rm_b);
@@ -33,4 +27,13 @@ make_helper(reg_81_v) {
 	}
 	test(0, "Invalid modrm reg domain");
 	return 0;
+}
+
+make_helper(cmp_i8_rm_v);
+make_helper(reg_83_v) {
+	GET_REG;
+	switch (m.reg) {
+		case 7: return cmp_i8_rm_v(eip);
+	}
+	FAIL_DISPATCH;
 }
