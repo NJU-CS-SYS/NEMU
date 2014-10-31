fdf16459871faf00ca992f9ee32cb43f25bfa059
jyy <njujiangyy@gmail.com>
2014-11-01 02:13:39 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  02:13:39 up 14:49,  3 users,  load average: 0.00, 0.02, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-2*[sshd---sshd---zsh---vim]      `-udevd---2*[udevd]

--- src/exec/control-transfer/jcc-template.h
+++ src/exec/control-transfer/jcc-template.h
@@ -3,9 +3,10 @@
 #include "cpu/modrm.h"
 extern char suffix;
 
+#define JCC_LEN_DEF int len = DATA_BYTE + (suffix == 'b' ? 1 : 2)
+
 #define JCC_COMMON_REL(condition, name) \
 int32_t imm = instr_fetch(eip + 1, DATA_BYTE);\
-int len = DATA_BYTE + (DATA_BYTE == 1 ? 1 : 2);\
 eip += imm;\
 if (suffix == 'w') eip &= 0x0000ffff;\
 if ((condition)) cpu.eip = eip;\
@@ -14,19 +15,22 @@ return len
 
 
 make_helper(concat(je_, SUFFIX)) {
+	JCC_LEN_DEF;
 	JCC_COMMON_REL(FLAG_VAL(ZF), je);
 }
 
 make_helper(concat(jmp_rel_, SUFFIX)) {
+	int len = 1 + DATA_BYTE;
 	JCC_COMMON_REL(1, jmp);
-
 }
 
 make_helper(concat(jbe_rel_, SUFFIX)) {
+	JCC_LEN_DEF;
 	JCC_COMMON_REL((FLAG_VAL(CF) || FLAG_VAL(ZF)), jbe);
 }
 
 make_helper(concat(jle_rel_, SUFFIX)) {
+	JCC_LEN_DEF;
 	JCC_COMMON_REL(((FLAG_VAL(OF) != FLAG_VAL(SF)) || FLAG_VAL(ZF)), jle);
 }
 
