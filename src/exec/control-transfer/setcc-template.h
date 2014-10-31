25faa4f79f129849057819fb39d6d16597f8381b
jyy <njujiangyy@gmail.com>
2014-10-31 20:30:34 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  20:30:34 up  9:06,  3 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-2*[sshd---sshd---zsh---vim]      `-udevd---2*[udevd]

--- src/exec/control-transfer/setcc-template.h
+++ src/exec/control-transfer/setcc-template.h
@@ -2,22 +2,24 @@
 #include "exec/template-start.h"
 #include "cpu/modrm.h"
 
-#define SET_CONDITION(x) \
+#define SET_CONDITION(x, name) \
 int len = 2;\
 ModR_M m;\
 m.val = instr_fetch(eip + 2, 1);\
 if (m.mod == 3) {\
 	REG(m.reg) = (x);\
+	print_asm(str(name) str(SUFFIX) " %%%s", REG_NAME(m.reg));\
 	return len + 1;\
 } else {\
 	swaddr_t addr;\
 	len += read_ModR_M(eip + 2, &addr);\
 	MEM_W(addr, (x));\
+	print_asm(str(name) str(SUFFIX) " %s", ModR_M_asm);\
 	return len;\
 }
 
 make_helper(set_ne_nz) {
-	SET_CONDITION(FLAG_VAL(ZF));
+	SET_CONDITION(FLAG_VAL(ZF), set_ne);
 }
 
 #include "exec/template-end.h"
