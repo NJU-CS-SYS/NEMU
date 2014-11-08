91ae19e319fb9dd092094c61958e4013dabcad49
jyy <njujiangyy@gmail.com>
2014-11-08 14:18:54 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  14:18:54 up  3:12,  2 users,  load average: 0.02, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---vim      |      `-sshd---sshd---zsh---make---sh-+-git      |                                      `-pstree      `-udevd---2*[udevd]

--- myloader/src/main.c
+++ myloader/src/main.c
@@ -1,9 +1,11 @@
 #include "trap.h"
+void loader();
 
 void init() {
 	/* Perform remaining initialization */
 
 	/* Load the program, and jump to it */
+	loader();
 
 	/* Should never reach here */
 	nemu_assert(0);
