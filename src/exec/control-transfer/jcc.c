8ec2f5a2b0342c8bfc87a969c472866ea62424da
jyy <njujiangyy@gmail.com>
2014-11-08 20:02:27 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  20:02:27 up  8:18,  2 users,  load average: 0.12, 0.04, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---tmux      |      `-sshd---sshd---zsh---make---sh-+-git      |                                      `-pstree      |-tmux---3*[zsh---vim]      `-udevd---2*[udevd]

--- src/exec/control-transfer/jcc.c
+++ src/exec/control-transfer/jcc.c
@@ -17,9 +17,30 @@
 
 extern char suffix;
 
-v_helper(je);
-v_helper(jbe_rel);
-v_helper(jl_rel);
-v_helper(jg_rel);
-v_helper(jmp_rel);
-v_helper(jne_rel);
+v_helper(je)
+v_helper(jne_rel)
+v_helper(jbe_rel)
+v_helper(jl_rel)
+v_helper(jle_rel)
+v_helper(jg_rel)
+v_helper(jge_rel)
+v_helper(jmp_rel)
+v_helper(jcxz_rel)
+v_helper(jecxz_rel)
+v_helper(ja_rel)
+v_helper(jae_rel)
+v_helper(jna_rel)
+v_helper(jnae_rel)
+v_helper(jnb_rel)
+v_helper(jnbe_rel)
+v_helper(jng_rel)
+v_helper(jnge_rel)
+v_helper(jnl_rel)
+v_helper(jnel_rel)
+v_helper(jno_rel)
+v_helper(jnp_rel)
+v_helper(jns_rel)
+v_helper(jnz_rel)
+v_helper(jo_rel)
+v_helper(jp_rel)
+v_helper(js_rel)
--- src/exec/control-transfer/jcc.h
+++ src/exec/control-transfer/jcc.h
@@ -1,12 +1,40 @@
 #ifndef __JCC_H__
 #define __JCC_H__
 
-all_helper(je);
-all_helper(jbe_rel);
-all_helper(jle_rel);
-all_helper(jl_rel);
 all_helper(jmp_rel);
-all_helper(jg_rel);
+
+all_helper(je);
 all_helper(jne_rel);
+
+all_helper(jb_rel);
+all_helper(jbe_rel);
+all_helper(jnb_rel);
+all_helper(jnbe_rel);
+
+all_helper(jl_rel);
+all_helper(jle_rel);
+all_helper(jnl_rel);
+all_helper(jnel_rel);
+
+all_helper(jg_rel);
+all_helper(jge_rel);
+all_helper(jng_rel);
+all_helper(jnge_rel);
+
+all_helper(ja_rel);
+all_helper(jae_rel);
+all_helper(jna_rel);
+all_helper(jnae_rel);
+
+all_helper(jcxz_rel);
+all_helper(jecxz_rel);
+
+all_helper(jno_rel);
+all_helper(jnp_rel);
+all_helper(jns_rel);
+all_helper(jnz_rel);
+all_helper(jo_rel);
+all_helper(jp_rel);
+all_helper(js_rel);
 
 #endif
