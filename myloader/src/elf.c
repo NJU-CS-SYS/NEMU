05268802ab80172f5f44a525fed2eb8d6e9a8bfc
jyy <njujiangyy@gmail.com>
2014-11-08 22:03:13 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  22:03:13 up 10:19,  2 users,  load average: 0.00, 0.04, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---tmux      |      `-sshd---sshd---zsh      |-tmux-+-zsh---vim      |      |-zsh---make---sh-+-git      |      |                 `-pstree      |      `-zsh      `-udevd---2*[udevd]

--- myloader/src/elf.c
+++ myloader/src/elf.c
@@ -9,7 +9,7 @@ void loader() {
 	uint16_t step = elf->e_phentsize;
 	uint16_t phnum = elf->e_phnum;
 
-	nemu_assert(phnum > 0);
+	nemu_assert(phnum == 0);
 	int i;
 	for (i = 0; i < 3; i++) {
 		/* Scan the program header table, loader each segment into memory */
