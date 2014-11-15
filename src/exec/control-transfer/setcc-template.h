e1e5db63a3d0351f30c9f678a7b6fb9e8038fb39
jyy <njujiangyy@gmail.com>
2014-11-15 10:50:12 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  10:50:12 up  1:29,  5 users,  load average: 0.04, 0.07, 0.06 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-3*[sshd---sshd---zsh---vim]      `-udevd---2*[udevd]

--- src/exec/control-transfer/setcc-template.h
+++ src/exec/control-transfer/setcc-template.h
@@ -3,20 +3,22 @@
 #include "cpu/modrm.h"
 
 #define SET_CONDITION(x, name) \
-int len = 2;\
-ModR_M m;\
-m.val = instr_fetch(eip + 2, 1);\
-if (m.mod == 3) {\
-	REG(m.reg) = (x);\
-	print_asm(str(name) " %%%s", REG_NAME(m.reg));\
-	return len + 1;\
-} else {\
-	swaddr_t addr;\
-	len += read_ModR_M(eip + 2, &addr);\
-	MEM_W(addr, (x));\
-	print_asm(str(name) " %s", ModR_M_asm);\
-	return len;\
-}
+	do {\
+		int len = 2;\
+		ModR_M m;\
+		m.val = instr_fetch(eip + 2, 1);\
+		if (m.mod == 3) {\
+			REG(m.reg) = (x);\
+			print_asm(str(name) " %%%s", REG_NAME(m.reg));\
+			return len + 1;\
+		} else {\
+			swaddr_t addr;\
+			len += read_ModR_M(eip + 2, &addr);\
+			MEM_W(addr, (x));\
+			print_asm(str(name) " %s", ModR_M_asm);\
+			return len;\
+		}\
+	} while (0)
 
 make_helper(set_ne_nz) {
 	SET_CONDITION(!FLAG_VAL(ZF), setne);
