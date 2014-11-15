30153b9c078eec2825dc64ae2c743966707329c6
jyy <njujiangyy@gmail.com>
2014-11-15 11:11:31 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  11:11:31 up  1:50,  5 users,  load average: 0.08, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-3*[sshd---sshd---zsh---vim]      `-udevd---2*[udevd]

--- src/exec/control-transfer/setcc-template.h
+++ src/exec/control-transfer/setcc-template.h
@@ -2,7 +2,7 @@
 #include "exec/template-start.h"
 #include "cpu/modrm.h"
 
-#define SET_CONDITION(x, name) \
+#define SET(x, name) \
 	do {\
 		int len = 2;\
 		ModR_M m;\
@@ -21,7 +21,53 @@
 	} while (0)
 
 make_helper(set_ne_nz) {
-	SET_CONDITION(!FLAG_VAL(ZF), setne);
+	SET(!FLAG_VAL(ZF), setne);
+}
+
+make_helper(set_o) {
+	SET(FLAG_VAL(OF), seto);
+}
+make_helper(set_no) {
+	SET(!FLAG_VAL(OF), setno);
+}
+make_helper(set_c) {
+	SET(FLAG_VAL(CF), setc);
+}
+make_helper(set_ae) {
+	SET(!FLAG_VAL(CF), setae);
+}
+make_helper(set_e) {
+	SET(FLAG_VAL(ZF), setae);
+}
+make_helper(set_be) {
+	SET((FLAG_VAL(CF) || FLAG_VAL(ZF)), setbe);
+}
+make_helper(set_a) {
+	SET(!(FLAG_VAL(CF) || FLAG_VAL(ZF)), seta);
+}
+make_helper(set_s) {
+	SET(FLAG_VAL(SF), sets);
+}
+make_helper(set_ns) {
+	SET(!FLAG_VAL(SF), setns);
+}
+make_helper(set_pe) {
+	SET(FLAG_VAL(PF), setpe);
+}
+make_helper(set_po) {
+	SET(!FLAG_VAL(PF), setpo);
+}
+make_helper(set_l) {
+	SET(FLAG_VAL(SF) != FLAG_VAL(OF), setl);
+}
+make_helper(set_ge) {
+	SET(FLAG_VAL(SF) == FLAG_VAL(OF), setge);
+}
+make_helper(set_le) {
+	SET(FLAG_VAL(ZF) && (FLAG_VAL(SF) != FLAG_VAL(OF)), setle);
+}
+make_helper(set_g) {
+	SET(!FLAG_VAL(ZF) && (FLAG_VAL(SF) == FLAG_VAL(OF)), setg);
 }
 
 #include "exec/template-end.h"
--- src/exec/control-transfer/setcc.h
+++ src/exec/control-transfer/setcc.h
@@ -2,5 +2,20 @@
 #define __SETCC_H__
 
 make_helper(set_ne_nz);
+make_helper(set_o);
+make_helper(set_no);
+make_helper(set_c);
+make_helper(set_ae);
+make_helper(set_e);
+make_helper(set_be);
+make_helper(set_a);
+make_helper(set_s);
+make_helper(set_ns);
+make_helper(set_pe);
+make_helper(set_po);
+make_helper(set_l);
+make_helper(set_ge);
+make_helper(set_le);
+make_helper(set_g);
 
 #endif
