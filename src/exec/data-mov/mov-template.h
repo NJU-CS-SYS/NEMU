a0e6ab0a6038bcc23b6794eac35dd083f878a7ed
jyy <njujiangyy@gmail.com>
2014-11-16 03:19:36 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  03:19:36 up 15:21,  5 users,  load average: 0.02, 0.03, 0.05 init-+-3*[zsh---vim]      |-zsh---make---sh-+-git      |                 `-pstree      |-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-3*[sshd---sshd---zsh---vim]      `-udevd---2*[udevd]

--- src/exec/data-mov/mov-template.h
+++ src/exec/data-mov/mov-template.h
@@ -177,7 +177,7 @@ make_helper(concat(movs_w2r_, SUFFIX)) {
 make_helper(concat(movs_m2m_, SUFFIX)) {
 	DATA_TYPE buf = MEM_R(REG(R_ESI));
 	MEM_W(REG(R_EDI), buf);
-	print_asm("movs");
+	print_asm("movs ES:%%%s,ES:%%%s", REG_NAME(R_ESI), REG_NAME(R_EDI));
 	return 1;
 }
 #include "exec/template-end.h"
