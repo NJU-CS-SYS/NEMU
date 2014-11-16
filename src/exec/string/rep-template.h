3885e2dd91648b4a45fe6305e508c734ef137e9f
jyy <njujiangyy@gmail.com>
2014-11-16 19:15:25 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  19:15:25 up 1 min,  1 user,  load average: 0.13, 0.05, 0.02 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh---make---sh-+-git      |                         `-pstree      |-rsyslogd---3*[{rsyslogd}]      |-sshd      `-udevd---2*[udevd]

--- src/exec/string/rep-template.h
+++ src/exec/string/rep-template.h
@@ -1,12 +1,14 @@
 #include "exec/helper.h"
 #include "cpu/modrm.h"
 #include "exec/template-start.h"
+#include "stdio.h"
 
 make_helper(exec);
 
 make_helper(concat(rep_, SUFFIX)) {
 	int len = 0;
 	print_asm("rep");
+	fflush(stdout);
 	while (REG(R_ECX) != 0) {
 		len = exec(eip+1);
 		REG(R_ECX) --;
