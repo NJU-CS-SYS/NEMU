c16cf08dbf0d492f1ebbccc057fcf17ad37a1433
jyy <njujiangyy@gmail.com>
2014-11-29 15:10:09 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  15:10:09 up  4:51,  4 users,  load average: 0.08, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---zsh---vim]      |      `-sshd---sshd---zsh---make---sh-+-git      |                                      `-pstree      `-udevd---2*[udevd]

--- src/memory/cache.c
+++ src/memory/cache.c
@@ -3,6 +3,8 @@
 #include "cpu/reg.h"
 #include "stdlib.h"
 
+uint32_t dram_read(hwaddr_t addr, size_t len);
+uint32_t dram_write(hwaddr_t addr, size_t len, uint32_t data);
 /* Simulate the (main) behavor of CACHE. Although this will lower the performace of NEMU,
  * it makes you clear about how CACHE is read/written.
  */
