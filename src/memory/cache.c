3480f090c408735c1467210f2c6367070c6eb685
jyy <njujiangyy@gmail.com>
2014-11-29 16:31:43 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  16:31:43 up  6:13,  4 users,  load average: 0.03, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---zsh---vim]      |      `-sshd---sshd---zsh---make---sh-+-git      |                                      `-pstree      `-udevd---2*[udevd]

--- src/memory/cache.c
+++ src/memory/cache.c
@@ -134,7 +134,7 @@ uint32_t read_cache(swaddr_t addr, size_t len) {
 	// search the cached data
 	int way;
 	for (way = 0; way < head->nr_way; way ++) {
-		Log("cached tag : %x, cmp tag : %x", head->cache[set][way].tag, tag);
+		Log("cached tag : %x, cmp tag : %x, valid : %x", head->cache[set][way].tag, tag, head->cache[set][way].valid);
 		if (head->cache[set][way].valid && head->cache[set][way].tag == tag) break;
 	}
 	// miss
