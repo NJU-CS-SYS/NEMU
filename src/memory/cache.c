6ea4d17a33b523be32cf5781910afd94eafd3815
jyy <njujiangyy@gmail.com>
2014-11-29 11:19:41 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  11:19:41 up  1:01,  4 users,  load average: 0.00, 0.02, 0.03 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---vim      |      |-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-sshd---sshd---zsh      `-udevd---2*[udevd]

---
+++ src/memory/cache.c
@@ -0,0 +1,18 @@
+#include "common.h"
+#include "lib/misc.h"
+#include "cpu/reg.h"
+
+/* Simulate the (main) behavor of CACHE. Although this will lower the performace of NEMU,
+ * it makes you clear about how CACHE is read/written.
+ */
+
+struct _cache_ {
+	int ***data;
+	int nr_set;
+	int nr_way;
+	int nr_block;
+	int **valid;
+	struct _cache_ *next;
+};
+typedef struct _cache_ cache;
+
