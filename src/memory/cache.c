8a37ad0df58135f3d12b357703eec685f6e5edba
jyy <njujiangyy@gmail.com>
2014-11-29 11:29:33 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  11:29:33 up  1:10,  4 users,  load average: 0.02, 0.02, 0.03 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---vim      |      |-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-sshd---sshd---zsh      `-udevd---2*[udevd]

--- src/memory/cache.c
+++ src/memory/cache.c
@@ -7,11 +7,11 @@
  */
 
 struct _cache_ {
-	int ***data;
+	uint8_t ***data;
 	int nr_set;
 	int nr_way;
 	int nr_block;
-	int **valid;
+	uint8_t **valid;
 	struct _cache_ *next;
 };
 typedef struct _cache_ cache;
@@ -24,14 +24,16 @@ bool create_cache(cache **ppcache, int x, int y, int z) {
 	pcache->nr_block = z;
 	pcache->next = NULL;
 
-	// allocate the mem for cache
+	// allocate the mem for data
 	int i, j;
-	pcache->data = (int***)malloc(sizeof(int**) * x);
+	pcache->data = (uint8_t***)malloc(sizeof(uint8_t**) * x);
 	for (i = 0; i < x; i ++) {
-		pcache->data[i] = (int**)malloc(sizeof(int*) * y);
+		pcache->data[i] = (uint8_t**)malloc(sizeof(uint8_t*) * y);
 		for (j = 0; j < y; j ++) {
-			pcache->data[i][j] = (int*)malloc(sizeof(int) * z);
+			pcache->data[i][j] = (uint8_t*)malloc(sizeof(uint8_t) * z);
 		}
 	}
+
+	// allocate the mem for valid
 	return 0;
 }
