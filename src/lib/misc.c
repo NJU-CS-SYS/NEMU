f968c9e2b4c2b5801322f4083140d25e6b3d1d1f
jyy <njujiangyy@gmail.com>
2014-11-30 00:47:19 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  00:47:19 up 13:58,  5 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---zsh]      |      |-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-sshd---sshd---zsh---vim      `-udevd---2*[udevd]

--- src/lib/misc.c
+++ src/lib/misc.c
@@ -3,11 +3,8 @@
 void memcpy_with_mask(void *dest, const void *src, size_t len, uint8_t *mask) {
 	int i;
 	for(i = 0; i < len; i ++) {
-		Log("lalala");
 		if(mask[i]) {
-			Log("src = %x", ((uint8_t *)src)[i]);
 			uint8_t temp = ((uint8_t *)src)[i];
-			Log("survive");
 			((uint8_t *)dest)[i] = temp;
 		}
 	}
