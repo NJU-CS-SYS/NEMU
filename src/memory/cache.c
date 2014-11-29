63070ee9e9c1c1e60291cf7b83b3ad6444b26e46
jyy <njujiangyy@gmail.com>
2014-11-29 20:57:00 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  20:57:00 up 10:38,  5 users,  load average: 0.08, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-3*[sshd---sshd---zsh---vim]      |      `-sshd---sshd---zsh---make---sh-+-git      |                                      `-pstree      `-udevd---2*[udevd]

--- src/memory/cache.c
+++ src/memory/cache.c
@@ -161,10 +161,10 @@ void sram_read(swaddr_t raw_addr, void* data) {
 }
 
 void sram_write(swaddr_t raw_addr, void *data, uint8_t *mask) {
-	swaddr_t addr = raw_addr & (~head->mask_block);
+	swaddr_t addr = raw_addr & (~BURST_MASK);
 	uint32_t tag = addr & head->mask_tag;
 	uint32_t set = (addr & head->mask_set) >> head->bit_block;
-	uint32_t offset = raw_addr & head->mask_block;
+	uint32_t offset = addr & head->mask_block;
 
 	Log("cache write: tag=%x, set=%x, offset=%x", tag, set, offset);
 	// search the block
