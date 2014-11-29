6c4be4c6bdbe89512e129dded874bb7103eb8fb8
jyy <njujiangyy@gmail.com>
2014-11-30 00:18:54 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  00:18:54 up 13:30,  5 users,  load average: 0.15, 0.09, 0.06 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---vim      |      |-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-2*[sshd---sshd---zsh]      `-udevd---2*[udevd]

--- src/memory/cache.c
+++ src/memory/cache.c
@@ -205,7 +205,6 @@ void sram_read(swaddr_t raw_addr, void* data, cache *p) {
 }
 
 void sram_write(swaddr_t raw_addr, void *data, uint8_t *mask, cache *p) {
-	Log("raw_addr=%x", raw_addr);
 	block** cache = p->cache;
 	swaddr_t addr = raw_addr & (~BURST_MASK);
 	uint32_t tag = addr & p->mask_tag;
@@ -244,6 +243,7 @@ uint32_t in_cache_read(swaddr_t addr, size_t len, cache *p) {
 }
 
 void in_cache_write(swaddr_t addr, size_t len, uint32_t data, cache *p) {
+	assert(p != NULL);
 	uint32_t offset = addr & BURST_MASK;
 	uint8_t temp[2 * BURST_LEN];
 	uint8_t mask[2 * BURST_LEN];
