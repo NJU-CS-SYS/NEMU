f583d2e272ceaa5f4c38ba62d1685a16d0b51024
jyy <njujiangyy@gmail.com>
2014-11-29 13:24:05 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  13:24:05 up  3:05,  4 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---vim      |      |-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-sshd---sshd---zsh      `-udevd---2*[udevd]

--- src/memory/cache.c
+++ src/memory/cache.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "lib/misc.h"
 #include "cpu/reg.h"
+#include "stdlib.h"
 
 /* Simulate the (main) behavor of CACHE. Although this will lower the performace of NEMU,
  * it makes you clear about how CACHE is read/written.
@@ -61,4 +62,21 @@ bool create_cache(cache **ppcache, int x, int y, int z) {
 	// init
 	init_cache(ppcache);
 	return true;
+}
+
+void delete_cache(cache *pcache) {
+	while (pcache != NULL) {
+		cache *ptemp = pcache;
+		pcache = pcache->next;
+
+		int i, j;
+		for (i = 0; i < ptemp->nr_set; i ++) {
+			for (j = 0; j < ptemp->nr_way; j ++) {
+				free(ptemp->data[i][j]);
+			}
+			free(ptemp->valid[i]);
+			free(ptemp->data[i]);
+		}
+		free(ptemp);
+	}
 }
