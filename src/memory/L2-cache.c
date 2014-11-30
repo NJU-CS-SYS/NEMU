5e6363353115a2356cc99cacb1d679124271a965
jyy <njujiangyy@gmail.com>
2014-11-30 12:03:09 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  12:03:09 up 17:43,  5 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-3*[sshd---sshd---zsh---vim]      |      `-sshd---sshd---zsh-+-make---sh-+-nemu      |                          |           `-tee      |                          `-make---sh-+-git      |                                      `-pstree      `-udevd---2*[udevd]

--- src/memory/L2-cache.c
+++ src/memory/L2-cache.c
@@ -66,11 +66,12 @@ static void L2_read(swaddr_t addr, void *data) {
 		}
 	}
 
-	if (way == NR_WAY) { // miss, write allocate
+	if (way == NR_WAY) { // miss, allocate
 		for (way = 0; way <	NR_WAY; way ++) // find empty block
 			if (!L2[set][way].valid)
 				break;
 		if (way == NR_WAY) { // cache full, replacement
+			assert(0);
 			way = rand() % NR_WAY; // random replacement
 			if (L2[set][way].dirty) { // write back
 				int i;
