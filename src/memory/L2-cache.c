315be12cfd8ebc417109e2aa19ee796c76a16567
jyy <njujiangyy@gmail.com>
2014-11-30 11:48:53 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  11:48:53 up 17:28,  5 users,  load average: 0.01, 0.02, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-3*[sshd---sshd---zsh---vim]      |      `-sshd---sshd---zsh-+-make---sh-+-nemu      |                          |           `-tee      |                          `-make---sh-+-git      |                                      `-pstree      `-udevd---2*[udevd]

--- src/memory/L2-cache.c
+++ src/memory/L2-cache.c
@@ -115,7 +115,7 @@ void L2_write(swaddr_t addr, void *data, uint8_t *mask) {
 		if (way == NR_WAY) { // cache full, replacement
 			way = rand() % NR_WAY; // random replacement
 			if (L2[set][way].dirty) { // write back
-				Log("hit");
+				assert(0);
 				int i;
 				hwaddr_t back_addr = (L2[set][way].tag << (SET_WIDTH + BLOCK_WIDTH)) | (set << BLOCK_WIDTH);
 				for (i = 0; i < NR_BLOCK; i ++)
