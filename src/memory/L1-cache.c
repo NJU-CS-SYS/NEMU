812aace837046b728741e0ff1de37ff10b891161
jyy <njujiangyy@gmail.com>
2014-11-30 11:36:20 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  11:36:20 up 17:16,  5 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-3*[sshd---sshd---zsh---vim]      |      `-sshd---sshd---zsh---make---sh-+-git      |                                      `-pstree      `-udevd---2*[udevd]

--- src/memory/L1-cache.c
+++ src/memory/L1-cache.c
@@ -59,6 +59,7 @@ static void L1_read(swaddr_t addr, void *data) {
 	uint32_t tag = temp.tag;
 	uint32_t offset = temp.offset;
 
+	Log("addr=%x, set=%x, tag=%x, offset=%x", addr, set, tag, offset);
 	uint32_t way;
 	for (way = 0; way < NR_WAY; way ++) { // search by tag
 		if (L1[set][way].tag == tag && L1[set][way].valid) {
