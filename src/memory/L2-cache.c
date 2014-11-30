f2c5c75f31c844bb52f80e8a124e9f964bc9555f
jyy <njujiangyy@gmail.com>
2014-11-30 12:14:02 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  12:14:02 up 17:54,  5 users,  load average: 0.02, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---zsh---vim]      |      |-sshd---sshd---zsh-+-make---sh-+-nemu      |      |                   |           `-tee      |      |                   `-make---sh-+-git      |      |                               `-pstree      |      `-sshd---sshd---zsh      `-udevd---2*[udevd]

--- src/memory/L2-cache.c
+++ src/memory/L2-cache.c
@@ -132,8 +132,6 @@ void L2_write(swaddr_t addr, void *data, uint8_t *mask) {
 				hwaddr_t back_addr = (L2[set][way].tag << (SET_WIDTH + BLOCK_WIDTH)) | (set << BLOCK_WIDTH);
 				for (i = 0; i < NR_BLOCK; i ++)
 					dram_write(back_addr + i, 1, L2[set][way].blk[i]);
-				for (i = 0; i < NR_BLOCK; i ++)
-					test(dram_read(back_addr + i, 1) == L2[set][way].blk[i], "write back wrong");
 			} 
 		}	
 		// write allocate
@@ -142,11 +140,12 @@ void L2_write(swaddr_t addr, void *data, uint8_t *mask) {
 		for (i = 0; i < NR_BLOCK; i ++)
 			L2[set][way].blk[i] = dram_read(load + i, 1);
 		L2[set][way].tag = tag;
+		Log("set = %x, way = %x, tag = %x", set, way, tag);
+		L2_print(load);
 
 	}
 	// burst write
 	L2[set][way].tag = tag;
-	L2_print(addr);
 	assert(0);
 	memcpy_with_mask(L2[set][way].blk + offset, data, BURST_LEN, mask);
 	L2[set][way].dirty = true;
