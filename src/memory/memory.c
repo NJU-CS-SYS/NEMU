c255c3b5ed1151966dbf1ad1305bd593c77b2af7
jyy <njujiangyy@gmail.com>
2014-12-07 19:41:20 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  19:41:20 up  4:28,  5 users,  load average: 0.01, 0.02, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-3*[sshd---sshd---zsh---vim]      `-udevd---2*[udevd]

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -1,57 +1,62 @@
 #include "common.h"
 #include "segment.h"
+#include "cpu/reg.h"
 uint32_t cache_read(swaddr_t addr, size_t len);
 uint32_t cache_write(swaddr_t addr, size_t len, uint32_t data);
-uint32_t L1_print(swaddr_t addr);
-uint32_t dram_read(hwaddr_t addr, size_t len);
-void dram_write(hwaddr_t addr, size_t len, uint32_t data);
 
 /* Memory accessing interfaces */
 
-uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	assert(len == 1 || len == 2 || len == 4);
-	return dram_read(addr, len);
-}
-
-void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	assert(len == 1 || len == 2 || len == 4);
-	dram_write(addr, len, data);
-}
-
-uint32_t swaddr_read(swaddr_t addr, size_t len) {
-	assert(len == 1 || len == 2 || len == 4);
-	uint32_t cache_data = cache_read(addr, len);
-#if 0
-	uint32_t hw_data = hwaddr_read(addr, len);
-	if(cache_data != hw_data) {
-		L1_print(addr);
-		int i = 0;
-		printf("real ram:\n");
-		for (i = 0; i < 64; i ++) {
-			printf(" %02x", hwaddr_read((addr&(~63u))+i,1));
-			if (i == 31) printf("\n");
-		}
-		printf("\n");
-		test(0, "cache wrong, addr = %x, len = %x, cache = %x, hw = %x", addr, len, cache_data, hw_data);
-	}
-#endif
-	return cache_data;
-}
-
-void swaddr_write(swaddr_t addr, size_t len, uint32_t data) {
-	assert(len == 1 || len == 2 || len == 4);
-	//hwaddr_write(addr, len, data);
-	cache_write(addr, len, data);
-}
-
-static uint32_t hwaddr_read_instr(hwaddr_t addr, size_t len) {
+uint32_t hwaddr_read(hwaddr_t addr, size_t len) 
+{
 	assert(len == 1 || len == 2 || len == 4);
 	return cache_read(addr, len);
 }
 
-uint32_t instr_fetch(swaddr_t addr, size_t len) {
+void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) 
+{
 	assert(len == 1 || len == 2 || len == 4);
-	Sreg = 1;
-	segment_translate(addr, len);
+	cache_write(addr, len, data);
+}
+
+static uint32_t lnaddr_read(lnaddr_t addr, size_t len) 
+{
+	assert(len == 1 || len == 2 || len == 4);
+	return hwaddr_read(addr, len);
+}
+
+static void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data)
+{
+	assert(len == 1 || len == 2 || len == 4);
+	return hwaddr_write(addr, len, data);
+}
+
+uint32_t swaddr_read(swaddr_t addr, size_t len) 
+{
+	assert(len == 1 || len == 2 || len == 4);
+	if (PE) {
+		addr = segment_translate(addr, len);
+	}
+	return lnaddr_read(addr, len);
+}
+
+void swaddr_write(swaddr_t addr, size_t len, uint32_t data) 
+{
+	assert(len == 1 || len == 2 || len == 4);
+	lnaddr_write(addr, len, data);
+}
+
+static uint32_t hwaddr_read_instr(hwaddr_t addr, size_t len) 
+{
+	assert(len == 1 || len == 2 || len == 4);
+	return cache_read(addr, len);
+}
+
+uint32_t instr_fetch(swaddr_t addr, size_t len) 
+{
+	assert(len == 1 || len == 2 || len == 4);
+	if (PE) {
+		Sreg = 1;
+		addr = segment_translate(addr, len);
+	}
 	return hwaddr_read_instr(addr, len);
 }
