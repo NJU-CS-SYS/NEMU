7610a3375632fac4c526fc15bc5d86f9d1ec861c
131220159-Wang Huizhe <huizhewang@outlook.com>
2014-12-08 21:14:13 +0800

    ss, ds, es override
    without make

--- src/exec/prefix/seg-override.c
+++ src/exec/prefix/seg-override.c
@@ -9,3 +9,24 @@ make_helper(cs_override) // CS segment override prefix
 	Log("CS segment override");
 	return 1 + exec(eip + 1);
 }
+
+make_helper(ss_override) // CS segment override prefix
+{
+	Sreg = SS;
+	Log("SS segment override");
+	return 1 + exec(eip + 1);
+}
+
+make_helper(ds_override) // CS segment override prefix
+{
+	Sreg = DS;
+	Log("CS segment override");
+	return 1 + exec(eip + 1);
+}
+
+make_helper(es_override) // CS segment override prefix
+{
+	Sreg = ES;
+	Log("ES segment override");
+	return 1 + exec(eip + 1);
+}
--- src/exec/prefix/seg-override.h
+++ src/exec/prefix/seg-override.h
@@ -1,3 +1,6 @@
 #include "exec/helper.h"
 
 make_helper(cs_override);
+make_helper(ss_override);
+make_helper(ds_override);
+make_helper(es_override);
