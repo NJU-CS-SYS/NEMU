7072400722681b65f05b534e5a04abb65e57e22c
jyy <njujiangyy@gmail.com>
2014-12-11 19:36:37 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  19:36:37 up 15 min,  4 users,  load average: 0.08, 0.04, 0.03 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      |-sshd---sshd---zsh---vim      |      `-sshd---sshd---zsh      `-udevd---2*[udevd]

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -22,6 +22,8 @@ void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data)
 static uint32_t lnaddr_read(lnaddr_t addr, size_t len) 
 {
 	assert(len == 1 || len == 2 || len == 4);
+	if (cpu.cr0.protect_enable) test(0,"pe");
+	if (cpu.cr0.paging) test(0,"paging");
 	if (cpu.cr0.protect_enable && cpu.cr0.paging) {
 		addr = page_translate(addr, len);
 	}
@@ -31,6 +33,8 @@ static uint32_t lnaddr_read(lnaddr_t addr, size_t len)
 static void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data)
 {
 	assert(len == 1 || len == 2 || len == 4);
+	if (cpu.cr0.protect_enable) test(0,"pe");
+	if (cpu.cr0.paging) test(0,"paging");
 	if (cpu.cr0.protect_enable && cpu.cr0.paging) {
 		addr = page_translate(addr, len);
 	}
