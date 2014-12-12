84214a364dc1207f10e0f93a3d9d52559f305d56
jyy <njujiangyy@gmail.com>
2014-12-12 22:01:47 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  22:01:47 up  9:50,  5 users,  load average: 0.12, 0.11, 0.07 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-git      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---zsh---vim]      |      |-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-sshd---sshd---zsh      `-udevd---2*[udevd]

--- src/memory/page.c
+++ src/memory/page.c
@@ -60,7 +60,7 @@ hwaddr_t page_translate(lnaddr_t addr, size_t len)
 	page_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * lnaddr.page, 4);
 	if (!page_entry.present) Test(0, "page fault: eip %#x, vaddr %#x", cpu.eip, addr);
 	hwaddr_t hwaddr = (page_entry.page_frame << 12) + lnaddr.offset;
-#if 1
+#if 0
 	Log("dir %x page table %x page %x", dir_addr, dir_entry.page_frame, page_entry.page_frame);
 	Log("lnaddr dir %x, page %x, offset %x", lnaddr.dir, lnaddr.page, lnaddr.offset);
 	Log("bit %02x", hwaddr_read(hwaddr, 1));
