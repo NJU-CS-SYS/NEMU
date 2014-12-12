9ab6cb2c37935a28c62ae93798fd4929c7ae94ec
jyy <njujiangyy@gmail.com>
2014-12-12 20:40:49 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  20:40:49 up  8:29,  5 users,  load average: 0.01, 0.06, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---zsh---vim]      |      |-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-sshd---sshd---zsh---2*[vim]      `-udevd---2*[udevd]

--- src/cpu/cpu-exec.c
+++ src/cpu/cpu-exec.c
@@ -82,7 +82,7 @@ void cpu_exec(volatile uint32_t n) {
 		cpu.eip += instr_len;
 
 		if(n_temp != -1 || (enable_debug && !quiet)) {
-			if ( !(0x100000 <= cpu.eip && 0x200000) ) {
+			if ( !(0x100000 <= cpu.eip && cpu.eip <= 0x200000) ) {
 				print_bin_instr(eip_temp, instr_len);
 				puts(assembly);
 			}
