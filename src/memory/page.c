dfb25d08d42b73160e0a2f7440a312a3bbb1ab7f
jyy <njujiangyy@gmail.com>
2014-12-16 18:03:55 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  18:03:55 up 48 min,  2 users,  load average: 0.04, 0.07, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---vim      |      `-sshd---sshd---zsh---make---sh-+-git      |                                      `-pstree      `-udevd---2*[udevd]

--- src/memory/page.c
+++ src/memory/page.c
@@ -8,10 +8,10 @@ void tlb_insert(lnaddr_t addr, PTE pte);
 hwaddr_t page_translate(lnaddr_t addr, size_t len)
 {
 	hwaddr_t hwaddr = -1;
+	hwaddr_t __hwaddr = -1;
 #if 0
 	if ( tlb_read(addr, &hwaddr) ) {
 		test(hwaddr != -1, "tlb hit and wrong");
-		return hwaddr;
 	}
 #endif
 	
@@ -29,12 +29,14 @@ hwaddr_t page_translate(lnaddr_t addr, size_t len)
 	if (!dir_entry.present) Test(0, "dir fault: eip %#x, vaddr %#x", cpu.eip, addr);
 	page_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * lnaddr.page, 4);
 	if (!page_entry.present) Test(0, "page fault: eip %#x, vaddr %#x", cpu.eip, addr);
-	hwaddr = (page_entry.page_frame << 12) + lnaddr.offset;
+	__hwaddr = (page_entry.page_frame << 12) + lnaddr.offset;
+	
+	test(__hwaddr == hwaddr || hwaddr == -1, "__hwaddr %x, hwaddr %x", __hwaddr, hwaddr);
 #if 0
 	Log("dir %x page table %x page %x", dir_addr, dir_entry.page_frame, page_entry.page_frame);
 	Log("lnaddr dir %x, page %x, offset %x", lnaddr.dir, lnaddr.page, lnaddr.offset);
 	Log("bit %02x", hwaddr_read(hwaddr, 1));
 #endif
 	tlb_insert(addr, page_entry);
-	return hwaddr;
+	return __hwaddr;
 }
