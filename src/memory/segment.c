15c49c4e4c4dfd40249cfd45b649f149fbdbad28
jyy <njujiangyy@gmail.com>
2014-12-23 05:03:41 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  05:03:41 up  3:07,  4 users,  load average: 0.16, 0.05, 0.06 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-3*[sshd---sshd---zsh---vim]      |      |-4*[sshd---sshd---sftp-server]      |      `-sshd---sshd---zsh---make---sh-+-git      |                                      `-pstree      `-udevd---2*[udevd]

--- src/memory/segment.c
+++ src/memory/segment.c
@@ -10,17 +10,15 @@ lnaddr_t segment_translate(swaddr_t addr)
 	hwaddr_t gdt_addr = cpu.gdtr.base;
 	uint16_t selector = SEG(Sreg);
 	uint16_t index = selector >> 3;
-	SegDesc descriptor;
-	uint32_t temp[2];
-	temp[0] = hwaddr_read(gdt_addr + index * sizeof(SegDesc), 4);
-	temp[1] = hwaddr_read(gdt_addr + index * sizeof(SegDesc) + 4, 4);
-	memcpy(&descriptor, temp, sizeof(SegDesc));
-	
-	lnaddr_t base = descriptor.base_31_24 << 24 | descriptor.base_23_16 << 16 | descriptor.base_15_0;
+	SegDesc desc;
+	desc.val_high = hwaddr_read(gdt_addr + index * sizeof(SegDesc), 4);
+	desc.val_low = hwaddr_read(gdt_addr + index * sizeof(SegDesc) + 4, 4);
+
+	lnaddr_t base = desc.base_31_24 << 24 | desc.base_23_16 << 16 | desc.base_15_0;
 	if (base != 0) {
 		Log("base %x", base);
-		Log("limit %x  %x", descriptor.limit_19_16, descriptor.limit_15_0);
-		Log("segment type　%x", descriptor.segment_type);
+		Log("limit %x  %x", desc.limit_19_16, desc.limit_15_0);
+		Log("segment type　%x", desc.segment_type);
 		Log("sel %x, index %x", selector, index);
 	}
 	return base + addr;
