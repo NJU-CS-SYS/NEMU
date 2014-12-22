5f748973fbe8f5cd359c9a492d4ea9950e50ccad
jyy <njujiangyy@gmail.com>
2014-12-23 03:20:19 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  03:20:19 up  1:24,  3 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      |-3*[sshd---sshd---sftp-server]      |      `-2*[sshd---sshd---zsh]      `-udevd---2*[udevd]

---
+++ src/cpu/int/idt.h
@@ -0,0 +1,17 @@
+typedef struct GateDescriptor {
+	uint32_t offset_15_0      : 16;
+	uint32_t segment          : 16;
+	uint32_t pad0             : 8;
+	uint32_t type             : 4;
+	uint32_t system           : 1;
+	uint32_t privilege_level  : 2;
+	uint32_t present          : 1;
+	uint32_t offset_31_16     : 16;
+} GateDesc;
+
+typedef struct TrapFrame {
+	uint32_t edi, esi, ebp, xxx, ebx, edx, ecx, eax;	// GPRs
+	int32_t irq;										// #irq
+	uint32_t error_code;								// error code
+	uint32_t eip, cs, eflags;							// execution state saved by hardware
+} TrapFrame;
