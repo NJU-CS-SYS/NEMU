f1527290c0102a42f6a067a8f19a470e7474b0d6
jyy <njujiangyy@gmail.com>
2014-12-23 03:30:53 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  03:30:53 up  1:35,  3 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      |-3*[sshd---sshd---sftp-server]      |      `-2*[sshd---sshd---zsh---vim]      `-udevd---2*[udevd]

--- src/memory/page.c
+++ src/memory/page.c
@@ -5,8 +5,10 @@
 uint32_t hwaddr_read(hwaddr_t addr, size_t len);
 bool tlb_read(lnaddr_t addr, hwaddr_t *hwaddr);
 void tlb_insert(lnaddr_t addr, PTE pte);
+
 hwaddr_t page_translate(lnaddr_t addr, size_t len)
 {
+	Log("vaddr is %x", addr);
 	hwaddr_t hwaddr = -1;
 	hwaddr_t __hwaddr = -1;
 #if 1
@@ -23,24 +25,13 @@ hwaddr_t page_translate(lnaddr_t addr, size_t len)
 
 	lnaddr.val = addr;
 
-	//uint32_t bound = lnaddr.offset + len - 1;
-	//if (bound > 0xfffu) Test(0,"override: bound %#x, offset %#x, len %d\n eip %#X vaddr %#X", bound, lnaddr.offset, len, cpu.eip, addr);
 	hwaddr_t dir_addr = cpu.cr3.page_directory_base << 12;
 	dir_entry.val = hwaddr_read(dir_addr + 4 * lnaddr.dir, 4);
 	if (!dir_entry.present) Test(0, "dir fault: eip %#x, vaddr %#x, dir %#x, page %#x, offset %#x", cpu.eip, addr, lnaddr.dir, lnaddr.page, lnaddr.offset);
-	//else Log("dir fault: eip %#x, vaddr %#x, dir %#x, page %#x, offset %#x", cpu.eip, addr, lnaddr.dir, lnaddr.page, lnaddr.offset);
 	page_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * lnaddr.page, 4);
 	if (!page_entry.present) Test(0, "page fault: eip %#x, vaddr %#x, dir %#x, page %#x, offset %#x", cpu.eip, addr, lnaddr.dir, lnaddr.page, lnaddr.offset);
-	//else Log("page fault: eip %#x, vaddr %#x, dir %#x, page %#x, offset %#x", cpu.eip, addr, lnaddr.dir, lnaddr.page, lnaddr.offset);
 	__hwaddr = (page_entry.page_frame << 12) + lnaddr.offset;
 
-	
-	//test(__hwaddr == hwaddr, "__hwaddr %x, hwaddr %x", __hwaddr, hwaddr);
-#if 0
-	Log("dir %x page table %x page %x", dir_addr, dir_entry.page_frame, page_entry.page_frame);
-	Log("lnaddr dir %x, page %x, offset %x", lnaddr.dir, lnaddr.page, lnaddr.offset);
-	Log("bit %02x", hwaddr_read(hwaddr, 1));
-#endif
 	tlb_insert(addr, page_entry);
 	return __hwaddr;
 }
