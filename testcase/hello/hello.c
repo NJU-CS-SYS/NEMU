22214148c0dcafe50ce289de672221750871bade
jyy <njujiangyy@gmail.com>
2014-12-23 02:18:49 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  02:18:49 up 23 min,  1 user,  load average: 0.08, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      `-sshd---sshd---sftp-server      `-udevd---2*[udevd]

---
+++ testcase/hello/hello.c
@@ -0,0 +1,13 @@
+const char fore[] = "IntroductionToComputerSystem";
+const char str[] = "Hello, world!\n";
+const char rear[] = "ProjectAssignment";
+
+int main()
+{
+	asm volatile ( "movl $4, %eax;"    // system call ID, 4 = SYS_write
+			       "movl $1, %ebx;"    // file descriptor, 1 = stdout
+				   "movl $str, %ecx;"  // buffer address
+				   "movl $20, %edx;"   // length
+				   "int $0x80");
+	return 0;
+}
