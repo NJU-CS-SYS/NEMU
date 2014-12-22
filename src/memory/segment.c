ddc6e494f86ee88bc6d1f3aceb4492716b0c8b6f
jyy <njujiangyy@gmail.com>
2014-12-23 04:44:51 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  04:44:51 up  2:49,  4 users,  load average: 0.08, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-3*[sshd---sshd---zsh---vim]      |      |-4*[sshd---sshd---sftp-server]      |      `-sshd---sshd---zsh---make---sh-+-git      |                                      `-pstree      `-udevd---2*[udevd]

--- src/memory/segment.c
+++ src/memory/segment.c
@@ -10,11 +10,10 @@ lnaddr_t segment_translate(swaddr_t addr)
 	hwaddr_t gdt_addr = cpu.gdtr.base;
 	uint16_t selector = SEG(Sreg);
 	uint16_t index = selector >> 3;
-	index = 0;
 	SegDesc descriptor;
 	uint32_t temp[2];
-	temp[0] = hwaddr_read(gdt_addr + index * sizeof(SegDesc), 4);
-	temp[1] = hwaddr_read(gdt_addr + index * sizeof(SegDesc) + 4, 4);
+	temp[1] = hwaddr_read(gdt_addr + index * sizeof(SegDesc), 4);
+	temp[0] = hwaddr_read(gdt_addr + index * sizeof(SegDesc) + 4, 4);
 	memcpy(&descriptor, temp, sizeof(SegDesc));
 	
 	lnaddr_t base = descriptor.base_31_24 << 24 | descriptor.base_23_16 << 16 | descriptor.base_15_0;
