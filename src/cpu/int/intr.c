b7e3c41b59a1f579ed88419774325242fb29b49c
jyy <njujiangyy@gmail.com>
2014-12-23 19:08:34 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  19:08:34 up 45 min,  3 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---vim      |      |-2*[sshd---sshd---sftp-server]      |      `-sshd---sshd---zsh---make---sh-+-git      |                                      `-pstree      `-udevd---2*[udevd]

--- src/cpu/int/intr.c
+++ src/cpu/int/intr.c
@@ -3,6 +3,7 @@
 
 #include "cpu/reg.h"
 #include "idt.h"
+#include "cpu/segment.h"
 
 extern jmp_buf jbuf;
 
@@ -13,8 +14,13 @@ void raise_intr(uint8_t NO)
 	 */	 
 	lnaddr_t idt_addr = cpu.idtr.base; 
 	size_t size = sizeof(GateDesc);
+	GateDesc desc;
+	*(uint32_t *)(&desc) = hwaddr_read(idt_addr + NO * size, 4);
+	*((uint32_t *)(&desc) + 1) = hwaddr_read(idt_addr + NO * size + 4, 4);
+	test((desc.type & 0x4) == 0x4, "failed in bit check");
 	Log("idt_addr %x", idt_addr);
 	Log("size %x", size);
+	Log("desc\n%x\n%x", *(uint32_t *)(&desc), *((uint32_t *)(&desc) + 1));
 	/* Jump back to cpu_exec() */
 	longjmp(jbuf, 1);
 }
