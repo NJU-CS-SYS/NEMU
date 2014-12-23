16be635d20b50fdef781cdb7941402511c2374a9
jyy <njujiangyy@gmail.com>
2014-12-24 00:21:53 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  00:21:53 up  5:32,  6 users,  load average: 0.01, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-4*[sshd---sshd---zsh---vim]      |      |-5*[sshd---sshd---sftp-server]      |      `-sshd---sshd---zsh-+-make---sh-+-git      |                          |           `-pstree      |                          `-vim      `-udevd---2*[udevd]

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -21,13 +21,13 @@ void do_syscall(TrapFrame *tf) {
 		 */
 		case 0: add_irq_handle(tf->ebx, (void*)tf->ecx); break;
 
-
 		case SYS_brk: sys_brk(tf); break;
 
 		/* TODO: Add more system calls. */
 
 		case SYS_write: asm volatile(".byte 0x82": :"a"(2), "c"(tf->ecx), "d"(tf->edx)); break;
-		//default: panic("Unhandled system call: id = %d", tf->eax);
+		
+		default: panic("Unhandled system call: id = %d", tf->eax);
 	}
 }
 
