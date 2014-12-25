19d9150ae56e5f250a931b9ec918162237f93b99
jyy <njujiangyy@gmail.com>
2014-12-25 23:16:12 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  23:16:12 up  1:34,  7 users,  load average: 0.17, 0.07, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh---vim      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      |-6*[sshd---sshd---sftp-server]      |      |-2*[sshd---sshd---zsh]      |      `-3*[sshd---sshd---zsh---vim]      `-udevd---2*[udevd]

--- src/exec/data-mov/cmov.c
+++ src/exec/data-mov/cmov.c
@@ -13,8 +13,29 @@ extern char suffix;
 	do{\
 		return ('l' == suffix ? concat(concat(cmov,x),_l)(eip) : concat(concat(cmov,x),_w)(eip));\
 	}while(0)
-
-make_helper(cmovg_v) {
-	return ('l' == suffix ? cmovg_l(eip) : cmovg_w(eip));
-}
+make_helper(cmovae_v) { make_cmov_v(ae); }
+make_helper(cmova_v) { make_cmov_v(a); }
+make_helper(cmovbe_v) { make_cmov_v(be); }
+make_helper(cmovb_v) { make_cmov_v(b); }
+make_helper(cmovcxz_v) { make_cmov_v(cxz); }
+make_helper(cmovecxz_v) { make_cmov_v(ecxz); }
+make_helper(cmove_v) { make_cmov_v(e); }
 make_helper(cmovge_v) { make_cmov_v(ge); }
+make_helper(cmovg_v) { make_cmov_v(g); }
+make_helper(cmovle_v) { make_cmov_v(le); }
+make_helper(cmovl_v) { make_cmov_v(l); }
+make_helper(cmovnae_v) { make_cmov_v(nae); }
+make_helper(cmovna_v) { make_cmov_v(na); }
+make_helper(cmovne_v) { make_cmov_v(ne); }
+make_helper(cmovnge_v) { make_cmov_v(nge); }
+make_helper(cmovng_v) { make_cmov_v(ng); }
+make_helper(cmovnl_v) { make_cmov_v(nl); }
+make_helper(cmovno_v) { make_cmov_v(no); }
+make_helper(cmovnp_v) { make_cmov_v(np); }
+make_helper(cmovns_v) { make_cmov_v(ns); }
+make_helper(cmovnz_v) { make_cmov_v(nz); }
+make_helper(cmovo_v) { make_cmov_v(o); }
+make_helper(cmovp_v) { make_cmov_v(p); }
+make_helper(cmovs_v) { make_cmov_v(s); }
+
+#undef make_cmov_v
--- src/exec/data-mov/cmov.h
+++ src/exec/data-mov/cmov.h
@@ -2,9 +2,34 @@
 #define __CMOV_H__
 
 #include "exec/helper.h"
+#define make_cmov_h(x)\
+	make_helper(concat(concat(cmov, x), _w));\
+	make_helper(concat(concat(cmov, x), _l));\
+	make_helper(concat(concat(cmov, x), _v));
 
-make_helper(cmovg_w);
-make_helper(cmovg_l);
-make_helper(cmovg_v);
-
+make_cmov_h(ae)
+make_cmov_h(a)
+make_cmov_h(be)
+make_cmov_h(b)
+make_cmov_h(cxz)
+make_cmov_h(ecxz)
+make_cmov_h(e)
+make_cmov_h(ge)
+make_cmov_h(g)
+make_cmov_h(le)
+make_cmov_h(l)
+make_cmov_h(nae)
+make_cmov_h(na)
+make_cmov_h(ne)
+make_cmov_h(nge)
+make_cmov_h(ng)
+make_cmov_h(nl)
+make_cmov_h(no)
+make_cmov_h(np)
+make_cmov_h(ns)
+make_cmov_h(nz)
+make_cmov_h(o)
+make_cmov_h(p)
+make_cmov_h(s)
+#undef make_cmov_h
 #endif
