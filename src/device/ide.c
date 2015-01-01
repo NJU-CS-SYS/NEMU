459b8510966bd6fceab681f59e789203469cb906
jyy <njujiangyy@gmail.com>
2015-01-01 20:37:32 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  20:37:32 up 1 day,  3:10,  5 users,  load average: 0.03, 0.02, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---4*[vim]      |      |-4*[sshd---sshd---sftp-server]      |      |-sshd---sshd---zsh---vim      |      |-sshd---sshd---zsh      |      `-sshd---sshd---zsh---make---sh-+-git      |                                      `-pstree      `-udevd---2*[udevd]

--- src/device/ide.c
+++ src/device/ide.c
@@ -60,7 +60,7 @@ void ide_io_handler(ioaddr_t addr, size_t len, bool is_write) {
 			}
 		}
 	}
-	printf("file pos : %ld, eip : %x, intr %x\n", ftell(disk_fp), cpu.eip, cpu.INTR);
+	//printf("file pos : %ld, eip : %x, intr %x\n", ftell(disk_fp), cpu.eip, cpu.INTR);
 }
 
 void init_ide() {
