bf055dd4b245e93261a4b3b1ee7c1d14dcfef6ef
jyy <njujiangyy@gmail.com>
2015-01-02 14:49:06 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  14:49:06 up 1 day, 11:26,  4 users,  load average: 0.20, 0.09, 0.07 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh-+-make---sh-+-git      |      |                   |           `-pstree      |      |                   `-vim      |      |-3*[sshd---sshd---sftp-server]      |      `-2*[sshd---sshd---zsh---vim]      `-udevd---2*[udevd]

--- src/cpu/int/intr.c
+++ src/cpu/int/intr.c
@@ -12,8 +12,7 @@ extern uint32_t main_entry;
 void raise_intr(uint8_t NO)
 {
 	/* Get gate descriptor */
-	if (NO == 0x20)
-		return;
+	//if (NO == 0x20) return;
 	lnaddr_t idt_addr = cpu.idtr.base;
 	size_t size = sizeof(GateDesc);
 	GateDesc desc;
