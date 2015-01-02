038ec992e76ebfc1bf942b8098a74672df5cf418
jyy <njujiangyy@gmail.com>
2015-01-02 15:29:12 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  15:29:12 up 1 day, 12:06,  5 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      |-4*[sshd---sshd---sftp-server]      |      |-sshd---sshd---zsh---2*[vim]      |      `-2*[sshd---sshd---zsh---vim]      `-udevd---2*[udevd]

--- src/exec/prefix/rep-template.h
+++ src/exec/prefix/rep-template.h
@@ -6,6 +6,10 @@ make_helper(exec);
 make_helper(concat(rep_, SUFFIX)) {
 	int len = 0;
 	Log("rep");
+	uint8_t next_opcode = instr_fetch(eip + 1, 1);
+	if (next_opcode == 0xc3) { // repz ret
+		return 1 + exec(eip + 1);
+	}
 	while (REG(R_ECX) != 0) {
 		len = exec(eip+1);
 		REG(R_ECX) --;
