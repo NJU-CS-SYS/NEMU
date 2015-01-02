5934961214f45863d0421d09001e0fa90892db2c
jyy <njujiangyy@gmail.com>
2015-01-02 14:42:42 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  14:42:42 up 1 day, 11:19,  4 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      |-3*[sshd---sshd---sftp-server]      |      |-sshd---sshd---zsh---vim      |      `-sshd---sshd---zsh      `-udevd---2*[udevd]

--- src/cpu/int/intr.c
+++ src/cpu/int/intr.c
@@ -18,6 +18,11 @@ void raise_intr(uint8_t NO)
 	*(uint32_t *)(&desc) = hwaddr_read(idt_addr + NO * size, 4);
 	*((uint32_t *)(&desc) + 1) = hwaddr_read(idt_addr + NO * size + 4, 4);
 
+	if ( !desc.present ) {
+		printf("unregistered intr %#x\n", NO);
+		return;
+	}
+
 	/* Store current infomation */
 	push(cpu.eflags);
 	push(cpu.cs);
@@ -28,10 +33,6 @@ void raise_intr(uint8_t NO)
 	cpu.eip = (desc.offset_31_16 << 16) | (desc.offset_15_0);
 
 	/* Debug */
-	test(desc.present, "failed in checking present bit of gate descriptor");
-	test((desc.type & 0x4) == 0x4, "failed in bit check");
-	Log("idt_addr %08x", idt_addr);
-	Log("size %x", size);
 	Log("desc\n%08x\n%08x", 
 			*(uint32_t *)(&desc), *((uint32_t *)(&desc) + 1));
 	Log("new cs %x", cpu.cs);
