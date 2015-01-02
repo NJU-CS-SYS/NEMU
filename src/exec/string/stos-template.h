4d16c3aabb86b873a8acfba0d1d60b5653c733b4
jyy <njujiangyy@gmail.com>
2015-01-02 14:09:50 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  14:09:50 up 1 day, 10:46,  4 users,  load average: 0.02, 0.02, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      |-3*[sshd---sshd---sftp-server]      |      `-2*[sshd---sshd---zsh---vim]      `-udevd---2*[udevd]

--- src/exec/string/stos-template.h
+++ src/exec/string/stos-template.h
@@ -10,7 +10,7 @@ make_helper(concat(stos_, SUFFIX)) // Store String Data
 	// TODO AddressSize variation
 	MEM_W(reg_l(R_DI),REG(R_EAX));
 	if (FLAG_VAL(DF)) reg_l(R_DI) -= DATA_BYTE;
-	else reg_w(R_DI) += DATA_BYTE;
+	else reg_l(R_DI) += DATA_BYTE;
 	print_asm("stos" str(SUFFIX));
 	return 1;
 }
