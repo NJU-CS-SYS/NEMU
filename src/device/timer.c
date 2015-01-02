3e0caf0046204c1503ee2fd67e00dc45c1d05e65
jyy <njujiangyy@gmail.com>
2015-01-02 18:01:19 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  18:01:19 up 1 day, 14:38,  5 users,  load average: 0.81, 0.29, 0.14 init-+-zsh---3*[vim]      |-zsh---make---sh-+-git      |                 `-pstree      |-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      |-4*[sshd---sshd---sftp-server]      |      |-2*[sshd---sshd---zsh---vim]      |      `-sshd---sshd---zsh      `-udevd---2*[udevd]

--- src/device/timer.c
+++ src/device/timer.c
@@ -2,14 +2,16 @@
 #include "ui/ui.h"
 
 #define TIMER_IRQ 0
-
+static uint64_t count;
 void timer_intr() {
 	if(nemu_state == RUNNING) {
-		printf("timer!\n");
+		count ++;
+		printf("timer! %llu\n", count);
 		fflush(stdout);
 		i8259_raise_intr(TIMER_IRQ);
 	}
 }
 
 void init_timer() {
+	count = 0;
 }
