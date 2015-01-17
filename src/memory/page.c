9fd5b070066c513cc6af33522ac4825899efa7db
jyy <njujiangyy@gmail.com>
2015-01-17 15:54:15 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  15:54:15 up  1:40,  6 users,  load average: 0.01, 0.09, 0.07 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-dbus-daemon      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---zsh      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---zsh---make---sh-+-git      |      |                               `-pstree      |      |-5*[sshd---sshd---sftp-server]      |      |-3*[sshd---sshd---zsh]      |      `-sshd---sshd---zsh---vim      `-udevd---2*[udevd]

--- src/memory/page.c
+++ src/memory/page.c
@@ -5,6 +5,8 @@
 
 #define TEST_FREE
 
+static int trigger = 1;
+
 uint32_t hwaddr_read(hwaddr_t addr, size_t len);
 bool tlb_read(lnaddr_t addr, hwaddr_t *hwaddr);
 void tlb_insert(lnaddr_t addr, PTE pte);
@@ -28,6 +30,7 @@ hwaddr_t page_translate(lnaddr_t addr, size_t len)
 	lnaddr.val = addr;
 
 	hwaddr_t dir_addr = cpu.cr3.page_directory_base << 12;
+
 	dir_entry.val = hwaddr_read(dir_addr + 4 * lnaddr.dir, 4);
 	if (!dir_entry.present) Test(0, "dir fault: eip %#x, vaddr %#x, dir %#x, page %#x, offset %#x", cpu.eip, addr, lnaddr.dir, lnaddr.page, lnaddr.offset);
 	page_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * lnaddr.page, 4);
@@ -37,11 +40,16 @@ hwaddr_t page_translate(lnaddr_t addr, size_t len)
 	tlb_insert(addr, page_entry);
 
 #ifdef TEST_FREE
-	lnaddr.val = 0x81440e0;
-	dir_addr = cpu.cr3.page_directory_base << 12;
-	dir_entry.val = hwaddr_read(dir_addr + 4 * lnaddr.dir, 4);
-	page_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * lnaddr.page, 4);
-	if (dir_entry.present & page_entry.present) Test(0, "Hit");
+	if (trigger) {
+		lnaddr.val = 0x81440e0;
+		dir_addr = cpu.cr3.page_directory_base << 12;
+		dir_entry.val = hwaddr_read(dir_addr + 4 * lnaddr.dir, 4);
+		page_entry.val = hwaddr_read((dir_entry.page_frame << 12) + 4 * lnaddr.page, 4);
+		if (dir_entry.present & page_entry.present) {
+			Test(0, "Hit");
+			trigger = 0;
+		}
+	}
 #endif
 
 	return __hwaddr;
