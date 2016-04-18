72477f01c810aeb59850495947eae140effb864f
Wonicon <conciselove@outlook.com>
2016-04-18 20:57:25 +0800

    Invalidate all output function by emtpy macro

--- include/common.h
+++ include/common.h
@@ -4,6 +4,41 @@
 #include "debug.h"
 #include "macro.h"
 
+#define SYS_LAB
+
+#ifdef SYS_LAB
+
+// Overload the print functions.
+
+inline void npc_fprintf(FILE *file, const char *fmt, ...) { }
+inline void npc_fputc(char ch, FILE *fp) { }
+inline void npc_fputs(const char *s, FILE *fp) { }
+
+#define fprintf npc_fprintf
+
+#ifdef printf
+#undef printf
+#endif
+
+#define printf(fmt, ...) npc_fprintf(stdout, fmt, ## __VA_ARGS__)
+
+#define fputs npc_fputs
+#define fputc npc_fputc
+
+#ifdef putc
+#undef putc
+#endif
+
+#define putc(ch, file) npc_fputc(ch, file)
+
+#ifdef puts
+#undef puts
+#endif
+
+#define puts(s) npc_fputs(s, stdout)
+
+#endif
+
 #include <stdint.h>
 #include <assert.h>
 #include <string.h>
@@ -13,6 +48,7 @@ typedef uint32_t hwaddr_t;
 typedef uint32_t lnaddr_t;
 typedef uint32_t swaddr_t;
 typedef int16_t ioaddr_t;
+
 
 /* boolean */
 #define false 0
