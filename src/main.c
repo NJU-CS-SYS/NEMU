8e6f8b68ca1f5bca2bfa49bbbfe9794f970345b7
Wonicon <conciselove@outlook.com>
2016-05-04 18:33:25 +0800

    简化入口函数，完善模拟环境的条件编译

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 
 #ifdef SYS_LAB
 #include "monitor.h"
-int global_use_std = 1;  // TODO cond-compile it
+int global_use_std = 1;  // 控制输出目标的开关，用于在模拟器初始化前只使用标准输出。
 Monitor monitor;
 #endif
 
@@ -15,15 +15,30 @@ void init_bp_pool();
 void init_regex();
 void init_signal();
 void init_L1();
-void reg_test();
 
 int enable_debug = false;
 int quiet = false;
 
+/**
+ * FIXME: 在这里临时定义系统原语，检查调用情况，尽可能删除。
+ */
+#ifdef DEPLOY
+ssize_t write(int fd, const void *buf, size_t count) { return 0; }
+ssize_t read(int fd, void *buf, size_t count) { return 0; }
+void *sbrk(void *i) { return NULL; }
+off_t lseek(int fd, off_t offset, int whence) { return 0; }
+int fstat(int fd, void *buf) { return 0; }
+int isatty(int fd) { return 0; }
+void close(int fd) {}
+#endif
+
 int main(int argc, char *argv[]) {
+
 #ifdef SYS_LAB
-    init_monitor(&monitor, "monitor/config");  // TODO cond-compile it and get from commandline
-    global_use_std = 0;  // TODO cond-compile it
+#ifndef DEPLOY
+    init_monitor(&monitor, "monitor/config");
+    global_use_std = 0;
+#endif
 #endif
 
     set_exec_file(argv[1]);
@@ -37,10 +52,6 @@ int main(int argc, char *argv[]) {
     init_bp_pool();
     load_table();
     init_L1();
-
-
-    /* Test whether the 'CPU_state' structure is organized correctly. */
-    reg_test();
 
     /* Initialization of device */
     init_device();
